5cbdabf2fd2ee1393aa4300327c3fa59340db451
LorenzoVela <lorenzo1.vela@mail.polimi.it>
2023-01-06 20:30:39 +0100

    06/01 1

--- pages/suggested_actions.py
+++ pages/suggested_actions.py
@@ -128,14 +128,57 @@ with body1:
             if len(correlationList) > 0:
                 for i in range(0, len(correlationList)):
                     if correlationList[i][0] in df.columns and correlationList[i][1] in df.columns: 
-                        strDropCorr =f"Columns **{correlationList[i][0]}** and  **{correlationList[i][1]}** are highly correlated. You're suggested to drop one of the two"
+                        if correlationSum[correlationList[i][0]] > correlationSum[correlationList[i][1]]:
+                            x = 0
+                            y = 1
+                        else:
+                            x = 1
+                            y = 0
+                        strDropCorr =f"Columns **{correlationList[i][0]}** and  **{correlationList[i][1]}** are highly correlated. You're suggested to drop **{correlationList[i][x]}** given the other correlation parameters between all the columns"
                         st.info(strDropCorr)
                         choice = st.radio("Select below", ["None", correlationList[i][0], correlationList[i][1]], index=0)
                         if choice != "None":    
                             droppedList.append(["column", choice])
             else:
-                st.success("All the columns are acceptably independent!")
+                st.success("All the columns have a correlation parameters between them very low. This means they're all useful and descriptive for the dataset!")
             st.markdown("---")
+
+        for col in df.columns:
+            if len(pd.unique(df[col])) == 1:
+                with st.expander("Useless column", expanded=True):
+                    strDropUnique = f"The column **{col}** has the same value for all the rows (that is '{df[col][1]}'). You're suggested to drop it because it doesn't add any information to the dataset"
+                    st.info(strDropUnique)
+                    choice1 = st.radio("Select below", ["Keep the column", "Drop the column"], index=0)
+                    if choice1 != "Keep the column":
+                        droppedList.append(["column", col])
+
+        for col in df.columns:
+            choiceNum = ""
+            choiceObj = ""
+            nullNum = df[col].isna().sum()
+            percentageNull = nullNum/len(df.index)*100
+            if percentageNull > 25:
+                with st.expander("Null values", expanded=True):
+                    if df[col].dtype == "object":
+                        strObjFill = f"The column **{col}** has " + str("%0.2f" %(percentageNull)) + f"% of null values. Given it's an object column, you're suggested to replace them with its mode value, that is '{df[col].mode()}'."
+                        st.info(strObjFill)
+                        choiceObj = st.radio("Select below", ["Don't replace null values", "Replace the null values with the mode"])
+                        if choiceObj != "Don't replace null values":
+                            droppedList.append(["nullReplacedObj", col])
+                    elif df[col].dtype == "float64":
+                        avgValue = "{:.2f}".format(report["variables"][col]["mean"])
+                        strNumFill = f"The column **{col}** has " + str("%0.2f" %(percentageNull)) + f"% of null values. Given it's a numeric column, you're suggested to replace them with its mean value, that is '{avgValue}'."
+                        st.info(strNumFill)
+                        choiceNum = st.radio("Select below", ["Don't replace null values", "Replace the null values with the mean"])
+                        if choiceObj != "Don't replace null values":
+                            droppedList.append(["nullReplacedNum", col])
+                    else:
+                        ()
+                        #st.error("Unrecognized col. type") 
+                st.markdown("---")
+
+
+
         st.session_state['droppedList'] = droppedList #update the array before doing the action    
         if len(droppedList) > 0:
             st.warning("After you apply the changes selected, a preview of the new dataset will be displayed")
@@ -147,7 +190,7 @@ with body1:
 with body2:
     if st.session_state['y'] == 1:
         
-        #here we should perform all the actions in "droppedList" and profile again the dataset
+        #here we should perform all the actions in "droppedList" and profile again the dataset if the user wants
         
         dfPreview = df.copy()
         successMessage = st.empty()
@@ -158,6 +201,13 @@ with body2:
             elif item[0] == "column":
                 if item[1] in dfPreview.columns:
                     dfPreview = dfPreview.drop(item[1], axis=1)
+
+            elif item[0] == "nullReplacedNum":
+                ()
+            elif item[0] == "nullReplacedObj":
+                ()
+
+
         st.write(dfPreview.head(50))
         st.warning("This action will be permanent")
         col1, col2, col3 = st.columns([1,1,10], gap='small')
